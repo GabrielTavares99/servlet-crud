d01c001bd391b628cafc8a7336061e508b785621
Gabriel Tavares <gabriel.tavares99@hotmail.com>
2017-12-29 20:35:18 -0200

    Fazendo requisição para cadastro de empresa

---
+++ src/main/java/br/com/alura/gerenciador/CadastraEmpresa.java
@@ -0,0 +1,24 @@
+package br.com.alura.gerenciador;
+
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/cadastraEmpresa")
+public class CadastraEmpresa extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nomeEmpresa = req.getParameter("nome");
+        Empresa empresa = new Empresa(nomeEmpresa);
+        new EmpresaDAO().adiciona(empresa);
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><body>Empresa cadastrada com sucesso: "+empresa.getNome()+"</body></html>");
+    }
+}
